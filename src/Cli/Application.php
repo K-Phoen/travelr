21891786468957ec06254e01781a3f7127ccc340
Kévin Gomez <contact@kevingomez.fr>
2018-04-16 15:36:13 +0200

    Add a quick way to dump the config referene for albums and global

--- src/Cli/Application.php
+++ src/Cli/Application.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace Travelr\Cli;
 
 use Silly\Application as Silly;
+use Symfony\Component\Config\Definition\Dumper\YamlReferenceDumper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Pimple\Psr11\Container as Psr11Container;
+use Travelr\Config\AlbumConfiguration;
+use Travelr\Config\GlobalConfiguration;
 
 class Application extends Silly
 {
@@ -32,6 +35,26 @@ class Application extends Silly
                 $this->service(Command\ListDirectories::class)->run($output, $webRoot);
             })
             ->descriptions('List available directories.');
+
+        $this
+            ->command('config:dump-reference [type]', function (string $type, OutputInterface $output): void {
+                switch ($type) {
+                    case 'album':
+                        $config = new AlbumConfiguration();
+                        break;
+                    case 'global':
+                        $config = new GlobalConfiguration();
+                        break;
+                    default:
+                        throw new \LogicException(sprintf('Unknown config type "%s". Known values: album, global.', $type));
+                }
+
+                $dumper = new YamlReferenceDumper();
+
+                $output->writeln($dumper->dump($config, 'travelr'));
+            })
+            ->descriptions('Dumps the default configuration for an album or the global configuration.')
+            ->defaults(['type' => 'global']);
 
         $this
             ->command('build [--config=] [webroot]', function (string $webRoot, OutputInterface $output, string $config = null): void {
